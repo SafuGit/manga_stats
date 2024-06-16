6d587ad0b57ba681aef4fb32edf1f19f7aff353d
safu.69 <Safwan55.ah@gmail.com>
2024-06-16 12:49:13 +0600

    trending now works ig

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from manga_api import MangaAPI
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 manga_api = MangaAPI()
 
 @app.route("/")
-def main() -> dict:
-    return manga_api.get_trending()
+def main():
+    return render_template("index.html")
+
+@app.route("/trending")
+def trending():
+    data = manga_api.get_trending()["data"][0:10]
+    top_10 = [manga["title"] for manga in data]
+    return render_template("trending.html", MangaList=top_10)
 
 if __name__ == "__main__":
     app.run(debug=True)
