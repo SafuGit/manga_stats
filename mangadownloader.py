d416de663bb14fc8a70807b49d7d60ea286e568e
safu.69 <Safwan55.ah@gmail.com>
2024-06-18 20:02:51 +0600

    added mangadownloader.py and mangascraper.py, removed mangareader.py (not necesarry, everything works now)

--- mangadownloader.py
+++ mangadownloader.py
@@ -1,20 +1,77 @@
-from enma import (Enma, CloudFlareConfig, ManganatoDownloader, Threaded, LocalStorage)
+import requests
+import img2pdf
+import os
 
-class MangaDownloader():
+# MANGANATO_ENDPOINT = "https://chapmanganato.to"
+MANGADEX_ENDPOINT = "https://api.mangadex.org/at-home/server"
+class MangaDownloader:
     def __init__(self) -> None:
-        self.enma: Enma = Enma()
-        self.enma.source_manager.set_source("manganato")
-        self.downloader = ManganatoDownloader()
-        self.local_storage = LocalStorage()
+        pass
 
-    def download_manga(self, Id: str, download_path, chapter) -> None:
-        self.manga = self.enma.get_manga(Id)
+    def download_chapter_imgs(self, id: str) -> None:
+        self.count = 0
+        response = requests.get(f"{MANGADEX_ENDPOINT}/{id}")
+        data = response.json()
+        base_url = data["baseUrl"]
+        url_hash = data["chapter"]["hash"]
+        for self.chapter in data["chapter"]["data"]:
+            download_url = f"{base_url}/data/{url_hash}/{self.chapter}"
+            self.session = requests.Session()
+            response = self.session.get(download_url, stream=True)
+            self.count += 1
+            os.makedirs("imgsrc", exist_ok=True)
+            with open(f"imgsrc/{self.count}.jpg", "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+        self.download_chapter_pdf()
+    def download_chapter_pdf(self):
+        with open(f"imgsrc/{self.count}.jpg", "rb") as f:
+            img_files = [open(f"imgsrc/{i}", "rb").read() for i in os.listdir("imgsrc") if i.endswith(".jpg")]
+            pdf_data = img2pdf.convert(img_files)
+            with open(f"{self.chapter}.pdf", "wb") as pdf_file:
+                pdf_file.write(pdf_data)
+            for file in os.listdir("imgsrc"):
+                os.remove(f"imgsrc/{file}")
 
-        if self.manga:
-            self.enma.download_chapter(
-                path=f"{download_path}/{self.manga.title.english}",
-                chapter=self.manga.chapters[chapter],
-                downloader=self.downloader,
-                saver=self.local_storage,
-                threaded=Threaded(use_threads=True, number_of_threads=5)
-            )
+manga = MangaDownloader()
+manga.download_chapter_imgs(id="5eae7e34-c932-4c6a-99a4-899cfe43c372")
+
+### TEMPORARILY DISABLED MANGANATO DUE TO CLOUDFLARE BLOCK ###
+
+#         self.session = requests.Session()
+#         self.session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+#         self.cookies: dict = {
+#             "content_server": "server2"
+#         }
+
+#     def query_download_links(self, id: str) -> None:
+#         response = self.session.get(f"{ENDPOINT}/{id}", cookies=self.cookies, impersonate="chrome110")
+#         soup = BeautifulSoup(response.text, "html.parser")
+
+#         chapter_links = [f"{chapter.find('a')['href']}" for chapter in soup.find_all("li", {"class": "a-h"})]
+#         chapters = [chapter.find('a')['href'].replace(f"{ENDPOINT}/{id}/", "") for chapter in soup.find_all("li", {"class": "a-h"})]
+
+#         # with ThreadPoolExecutor(max_workers=20) as executor:
+#         #     executor.map(self.download_chapter, (chapter_links[0], chapters[0]))
+
+#         print(chapter_links[10])
+#         self.download_chapter(chapter_links[10], chapters[10])
+
+#     def download_chapters(self, link, manga_chapter):
+#         chapter_link = link
+#         chapter = manga_chapter
+#         response = self.session.get(chapter_link, cookies=self.cookies, impersonate="chrome110")
+#         soup = BeautifulSoup(response.text, "html.parser")
+
+#         for img in soup.find_all("img"):
+#             time.sleep(3)
+#             src = img.get("src")
+#             print(src)
+#             response = self.session.get(src, stream=True, cookies=self.cookies, impersonate="chrome110")
+#             print(response)
+#             print(self.session.headers)
+#             with open(f"{chapter}.jpg", "wb") as f:
+#                 for chunk in response.iter_content(chunk_size=1024):
+#                     if chunk:
+#                         f.write(chunk)
--- mangascraper.py
+++ mangascraper.py
@@ -1,26 +1,36 @@
 import requests
-from bs4 import BeautifulSoup
+import json
 
-ENDPOINT = "https://manganato.com/search/story"
-BASE_ENDPOINT = "https://chapmanganato.to/"
+MANGADEX_ENDPOINT = "https://api.mangadex.org"
 
 class Scraper():
     def __init__(self) -> None:
         pass
 
-    def get_mangas(self, query: str):
-        filtered_query: str = query.replace(" ", "_") 
-        self.response = requests.get(f"{ENDPOINT}/{filtered_query}")
-        self.soup = BeautifulSoup(self.response.text, "html.parser")
-        self.data = self.soup.find_all("div", {"class": "search-story-item"})
+    def get_mangas_mangadex(self, query: str):
+        response = requests.get(f"{MANGADEX_ENDPOINT}/manga", params={"title": query,})
+        return {manga["attributes"]["title"]["en"]: manga["id"] for manga in response.json()["data"]}
+    def get_chapters_mangadex(self, manga_id: str):
+        response = requests.get(f"{MANGADEX_ENDPOINT}/manga/{manga_id}/feed", params={"translatedLanguage[]": "en", "limit": 500})
+        data = response.json()
+        chapters = {item['attributes']["chapter"]: item["id"] for item in data["data"]}
+        return chapters
 
-        title: str = ""
-        manga_id: str = ""
-        manga_dict: dict = {}
+### TEMPORARILY DISABLED MANGANATO DUE TO CLOUDFLARE BLOCK ###
 
-        for manga in self.data:
-            soup = BeautifulSoup(str(manga), "html.parser")
-            title = soup.find("h3").find("a")["title"]
-            manga_id = soup.find("h3").find("a")["href"].replace(BASE_ENDPOINT, "")
-            manga_dict.update({title: manga_id})
-        return manga_dict
+    # def get_mangas_manganato(self, query: str):
+    #     filtered_query: str = query.replace(" ", "_") 
+    #     self.response = requests.get(f"{ENDPOINT}/{filtered_query}")
+    #     self.soup = BeautifulSoup(self.response.text, "html.parser")
+    #     self.data = self.soup.find_all("div", {"class": "search-story-item"})
+
+    #     title: str = ""
+    #     manga_id: str = ""
+    #     manga_dict: dict = {}
+
+    #     for manga in self.data:
+    #         soup = BeautifulSoup(str(manga), "html.parser")
+    #         title = soup.find("h3").find("a")["title"]
+    #         manga_id = soup.find("h3").find("a")["href"]
+    #         manga_dict.update({title: manga_id})
+    #     return manga_dict
